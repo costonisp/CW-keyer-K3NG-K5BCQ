b34c625e74bca02c4e16df8c5bc33560982c1e0d
co sto <10115135+costonisp@users.noreply.github.com>
2019-10-11 13:36:15 +0200

    Update keyer_features_and_options_k5bcq.h

--- k3ng_keyer/keyer_features_and_options_k5bcq.h
+++ k3ng_keyer/keyer_features_and_options_k5bcq.h
@@ -51,7 +51,7 @@
 // #define FEATURE_SD_CARD_SUPPORT
 
 // #define FEATURE_COMMAND_LINE_INTERFACE_ON_SECONDARY_PORT     // Activate the Command Line interface on the secondary serial port
-#define OPTION_PRIMARY_SERIAL_PORT_DEFAULT_WINKEY_EMULATION  // Use when activating both FEATURE_WINKEY_EMULATION and FEATURE_COMMAND_LINE_INTERFACE
+// #define OPTION_PRIMARY_SERIAL_PORT_DEFAULT_WINKEY_EMULATION  // Use when activating both FEATURE_WINKEY_EMULATION and FEATURE_COMMAND_LINE_INTERFACE
                                                              //    simultaneously.  This will make Winkey emulation be the default at boot up;
                                                              //    hold command button down at boot up to activate CLI mode
 
